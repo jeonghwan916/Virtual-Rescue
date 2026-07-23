c77e76dae08aaae4c89412f67b2fc8bc261262f2
westkitestone <westkitestone@gmail.com>
2026-07-23 15:05:37 +0900

    feat: add base Lobby UI System

--- Assets/02_Scripts/Lobby/StageData.cs
+++ Assets/02_Scripts/Lobby/StageData.cs
@@ -5,9 +5,6 @@ namespace VirtualRescue.Lobby
     [CreateAssetMenu(fileName = "StageData", menuName = "Virtual Rescue/Lobby/Stage Data")]
     public class StageData : ScriptableObject
     {
-        [Header("Button")]
-        [SerializeField] private int _buttonNumber;
-
         [Header("UI")]
         [SerializeField] private Sprite _image;
         [SerializeField] private string _primaryText;
@@ -15,11 +12,12 @@ namespace VirtualRescue.Lobby
 
         [Header("Scene")]
         [SerializeField] private string _sceneKey;
+        [SerializeField] private int _sceneBuildIndex = -1;
 
-        public int ButtonNumber => _buttonNumber;
         public Sprite Image => _image;
         public string PrimaryText => _primaryText;
         public string SecondaryText => _secondaryText;
         public string SceneKey => _sceneKey;
+        public int SceneBuildIndex => _sceneBuildIndex;
     }
 }
