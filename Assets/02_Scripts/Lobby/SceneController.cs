3b24d69186de1ed56bfa97e5beda9f1d796bc3ce
westkitestone <westkitestone@gmail.com>
2026-07-23 16:04:23 +0900

    feat: after 1st build-test (Only on map)

--- Assets/02_Scripts/Lobby/SceneController.cs
+++ Assets/02_Scripts/Lobby/SceneController.cs
@@ -19,7 +19,16 @@ namespace VirtualRescue.Lobby
             if (!string.IsNullOrWhiteSpace(_selectedSceneKey))
             {
                 Debug.Log("Loading... " + _selectedSceneKey);
-                SceneManager.LoadScene(_selectedSceneKey);
+                SceneManager.LoadScene(_selectedSceneKey, LoadSceneMode.Single);
+                
+                // 2026.07.23 / HyungJun / 빌드테스트용 코드
+                Debug.Log($"[{name}] 빌드테스트용 전체 씬 로드");
+                SceneManager.LoadScene("BedRoom", LoadSceneMode.Additive);
+                SceneManager.LoadScene("BedRoom2", LoadSceneMode.Additive);
+                SceneManager.LoadScene("Hallway&Stair", LoadSceneMode.Additive);
+                SceneManager.LoadScene("Kitchen&LivingRoom", LoadSceneMode.Additive);
+                SceneManager.LoadScene("VestibuleRoom", LoadSceneMode.Additive);
+                SceneManager.LoadScene("S_Env", LoadSceneMode.Additive);
                 return;
             }
 
