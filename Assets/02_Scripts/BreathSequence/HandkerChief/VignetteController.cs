5bcbffe94688fecfb6ffa3a07bdd5290507e5b28
westkitestone <westkitestone@gmail.com>
2026-07-24 23:04:07 +0900

    fix: Vignette Script

--- Assets/02_Scripts/BreathSequence/HandkerChief/VignetteController.cs
+++ Assets/02_Scripts/BreathSequence/HandkerChief/VignetteController.cs
@@ -5,9 +5,11 @@ using System.Collections;
 public class VignetteController : MonoBehaviour
 {
     static readonly int ApertureSizeId = Shader.PropertyToID("_ApertureSize");
+    static readonly int FeatheringEffectId = Shader.PropertyToID("_FeatheringEffect");
 
     [SerializeField] MeshRenderer vignetteRenderer;
     [SerializeField, Range(0f, 1f)] float startApertureSize = 0.5f;
+    [SerializeField, Range(0f, 1f)] float featheringEffect = 0.3f;
     [SerializeField, Min(0f)] float wipeOutDuration = 0.3f;
 
     MaterialPropertyBlock propertyBlock;
@@ -20,6 +22,7 @@ public class VignetteController : MonoBehaviour
             vignetteRenderer = GetComponent<MeshRenderer>();
 
         propertyBlock = new MaterialPropertyBlock();
+        WipeIn();
         //SetApertureSize(startApertureSize);
     }
 
@@ -29,6 +32,14 @@ public class VignetteController : MonoBehaviour
             StopCoroutine(wipeOutRoutine);
 
         wipeOutRoutine = StartCoroutine(WipeOutRoutine());
+    }
+
+    public void WipeIn()
+    {
+        if (wipeOutRoutine != null)
+            StopCoroutine(wipeOutRoutine);
+
+        wipeOutRoutine = StartCoroutine(WipeInRoutine());
     }
 
     IEnumerator WipeOutRoutine()
@@ -57,6 +68,32 @@ public class VignetteController : MonoBehaviour
         wipeOutRoutine = null;
     }
 
+    IEnumerator WipeInRoutine()
+    {
+        var startSize = currentApertureSize;
+
+        if (wipeOutDuration <= 0f)
+        {
+            SetApertureSize(startApertureSize);
+            wipeOutRoutine = null;
+            yield break;
+        }
+
+        var elapsed = 0f;
+        while (elapsed < wipeOutDuration)
+        {
+            elapsed += Time.deltaTime;
+            var t = Mathf.Clamp01(elapsed / wipeOutDuration);
+            var easedT = 1f - (1f - t) * (1f - t);
+
+            SetApertureSize(Mathf.Lerp(startSize, startApertureSize, easedT));
+            yield return null;
+        }
+
+        SetApertureSize(startApertureSize);
+        wipeOutRoutine = null;
+    }
+
     public void SetApertureSize(float apertureSize)
     {
         currentApertureSize = Mathf.Clamp01(apertureSize);
@@ -66,6 +103,7 @@ public class VignetteController : MonoBehaviour
 
         vignetteRenderer.GetPropertyBlock(propertyBlock);
         propertyBlock.SetFloat(ApertureSizeId, currentApertureSize);
+        propertyBlock.SetFloat(FeatheringEffectId, featheringEffect);
         vignetteRenderer.SetPropertyBlock(propertyBlock);
     }
 }
--- Assets/02_Scripts/BreathSequence/HandkerChief/VignetteTrigger.cs
+++ Assets/02_Scripts/BreathSequence/HandkerChief/VignetteTrigger.cs
@@ -5,13 +5,15 @@ public class VignetteTrigger : MonoBehaviour
 {
     [SerializeField] private VignetteController _vignetteController;
     [SerializeField, Range(0f, 1f)] float _startApertureSize = 0.5f;
-
+    [SerializeField] private bool _hasTriggerEntered = false;
+    
     
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !_hasTriggerEntered)
         {
-            _vignetteController.SetApertureSize(_startApertureSize);
+            _hasTriggerEntered = true;
+            _vignetteController.WipeIn();
         }
     }
 }
