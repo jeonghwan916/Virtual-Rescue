6014c77cd45c05f533b2e6d34d684cf68acb7e02
westkitestone <westkitestone@gmail.com>
2026-07-24 18:28:45 +0900

    feat: re-write FireHose Script

--- Assets/02_Scripts/FireSequence/FireTool/FireHose/HoseButton.cs
+++ Assets/02_Scripts/FireSequence/FireTool/FireHose/HoseButton.cs
@@ -3,17 +3,28 @@ using UnityEngine;
 
 public class HoseButton : MonoBehaviour
 {
+    [SerializeField] private bool _isLeverEnable = false;
+    
     public event Action OnButtonPressed;
+    public event Action OnButtonUnPressed;
 
     // todo : Temporary - Init
     private void Start()
     {
-        Press();
+        LeverEnabled();
     }
 
-    public void Press()
+    public void LeverEnabled()
     {
-        Debug.Log($"{nameof(HoseButton)} pressed.", this);
+        Debug.Log($"{nameof(HoseButton)} enabled.", this);
+        _isLeverEnable = true;
         OnButtonPressed?.Invoke();
+    }
+
+    public void LeverDisabled()
+    {
+        Debug.Log($"{nameof(HoseButton)} disabled.", this);
+        _isLeverEnable = false;
+        OnButtonUnPressed?.Invoke();
     }
 }
