90e0d5fb2c5e2e0b887d70f60c68d2d0859026ae
KimJeongHwan <jeonghwan916@gmail.com>
2026-07-27 18:12:45 +0900

    fix: 사운드 버그 수정

--- Assets/02_Scripts/Missions03/FireExtinguisherQuestManager.cs
+++ Assets/02_Scripts/Missions03/FireExtinguisherQuestManager.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using VirtualRescue.DialogueSystem;
-using VirtualRescue.Interactions;
 
 namespace VirtualRescue.Missions03
 {
@@ -19,7 +18,6 @@ namespace VirtualRescue.Missions03
         [SerializeField] private DialogueManager _dialogueManager;
         [SerializeField] private FireExtinguisher _fireExtinguisher;
         [SerializeField] private FireObject _fireObject;
-        [SerializeField] private FireBellButton _fireBellButton;
 
         [Header("Dialogue Groups")]
         [SerializeField] private string _startDialogueGroup = "quest03_start";
@@ -46,11 +44,6 @@ namespace VirtualRescue.Missions03
             if (_fireObject == null)
             {
                 _fireObject = GetComponentInChildren<FireObject>(true);
-            }
-
-            if (_fireBellButton == null)
-            {
-                _fireBellButton = FindFirstObjectByType<FireBellButton>();
             }
 
             ValidateReferences();
@@ -93,7 +86,6 @@ namespace VirtualRescue.Missions03
 
             _dialogueManager.Stop();
             _dialogueManager.PlayGroup(_startDialogueGroup);
-            _fireBellButton?.StartBell();
             _currentStep = FireExtinguisherQuestStep.WaitingForExtinguisherGrab;
             return true;
         }
