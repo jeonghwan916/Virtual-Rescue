6ef1e4d6f9a0eee3dcfda289f5c25ec82e34c1ed
westkitestone <westkitestone@gmail.com>
2026-08-13 17:25:10 +0900

    CallendarController.cs 재수정 & 달력 위치 조정

--- Assets/02_Scripts/00_Core/Loop/ProgressObject/CallendarController.cs
+++ Assets/02_Scripts/00_Core/Loop/ProgressObject/CallendarController.cs
@@ -25,20 +25,21 @@ public class CallendarController : MonoBehaviour
     private void ChangeCallendar(int currentDay)
     {
         // 모든 집 모듈 + 상황 씬 로딩 + 페이드인까지 끝난 뒤 실행
-        if (currentDay != 1)
+        for (int i = 0; i < _callendars.Length; i++)
         {
-            for (int i = 0; i < _callendars.Length; i++)
-            {
-                if (_callendars[i].activeSelf) _callendars[i].SetActive(false);
-            }
-            
-            if (currentDay == 2) _callendars[1].SetActive(true);
-            else if (currentDay == 3) _callendars[2].SetActive(true);
-            else if (currentDay == 4) _callendars[3].SetActive(true);
-            else if (currentDay == 5) _callendars[4].SetActive(true);
-            else if (currentDay == 6) _callendars[5].SetActive(true);
-            else if (currentDay == 7) _callendars[6].SetActive(true);
-            else if (currentDay == 8) _callendars[7].SetActive(true);
+            if (_callendars[i].activeSelf) _callendars[i].SetActive(false);
         }
+            
+        _callendars[currentDay - 1].SetActive(true);
+        /*
+        if (currentDay == 1) _callendars[0].SetActive(true);
+        else if (currentDay == 2) _callendars[1].SetActive(true);
+        else if (currentDay == 3) _callendars[2].SetActive(true);
+        else if (currentDay == 4) _callendars[3].SetActive(true);
+        else if (currentDay == 5) _callendars[4].SetActive(true);
+        else if (currentDay == 6) _callendars[5].SetActive(true);
+        else if (currentDay == 7) _callendars[6].SetActive(true);
+        else if (currentDay == 8) _callendars[7].SetActive(true);
+        */
     }
 }
