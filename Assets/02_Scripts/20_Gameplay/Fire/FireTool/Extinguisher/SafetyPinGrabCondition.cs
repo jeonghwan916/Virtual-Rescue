3d60ddf69893505a9313cda50d4d220fb8adecfe
westkitestone <westkitestone@gmail.com>
2026-08-21 10:37:47 +0900

    소화기 프리팹 일부 수정

--- Assets/02_Scripts/20_Gameplay/Fire/FireTool/Extinguisher/SafetyPinGrabCondition.cs
+++ Assets/02_Scripts/20_Gameplay/Fire/FireTool/Extinguisher/SafetyPinGrabCondition.cs
@@ -8,6 +8,7 @@ using UnityEngine.XR.Interaction.Toolkit.Interactors;
 public class SafetyPinGrabCondition : MonoBehaviour, IXRSelectFilter
 {
     [SerializeField] private XRGrabInteractable _extinguisherGrab;
+    [SerializeField, Min(0f)] private float _touchDistance = 0.1f;
 
     public bool canProcess => isActiveAndEnabled;
 
@@ -16,6 +17,39 @@ public class SafetyPinGrabCondition : MonoBehaviour, IXRSelectFilter
         if (interactor is XRSocketInteractor)
             return true;
 
-        return _extinguisherGrab != null && _extinguisherGrab.isSelected;
+        if (_extinguisherGrab == null ||
+            _extinguisherGrab.isSelected ||
+            interactor.handedness == InteractorHandedness.None)
+            return false;
+
+        float touchDistanceSqr = _touchDistance * _touchDistance;
+
+        foreach (IXRHoverInteractor hoveringInteractor in _extinguisherGrab.interactorsHovering)
+        {
+            if (hoveringInteractor.handedness == InteractorHandedness.None ||
+                hoveringInteractor.handedness == interactor.handedness)
+            {
+                continue;
+            }
+
+            Transform attachTransform = hoveringInteractor.GetAttachTransform(_extinguisherGrab);
+            if (attachTransform == null)
+                continue;
+
+            foreach (Collider bodyCollider in _extinguisherGrab.colliders)
+            {
+                if (bodyCollider == null || !bodyCollider.enabled)
+                    continue;
+
+                Vector3 closestPoint = bodyCollider.ClosestPoint(attachTransform.position);
+                if ((closestPoint - attachTransform.position).sqrMagnitude <=
+                    touchDistanceSqr)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
